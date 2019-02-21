2e899849f3478842e4538568bb78924f3b032017
hzhongl <1327415207@qq.com>
2019-02-21 16:21:03 +0800

    网关

---
+++ src/test/java/com/goshop/zuul/GoShopZuulApplicationTests.java
@@ -0,0 +1,17 @@
+package com.goshop.zuul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GoShopZuulApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
